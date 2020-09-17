60a8615167658820b1a40299549c768199893f5d
marcos-estrela <marcos.estrela@hotmail.com>
2020-09-17 01:05:46 -0300

    Change router structure

--- src/_helpers/router.js
+++ src/_helpers/router.js
@@ -1,36 +1,85 @@
-import Vue from 'vue';
-import Router from 'vue-router';
+import Vue from "vue";
+import Router from "vue-router";
 
-import DefaultPage from '../pages/Default'
+import DefaultPage from "../pages/Default";
 
 // import dataConfig data.from '../dataConfig.data.json'
-import dataConfig from '../config'
+import dataConfig from "../config";
 
 Vue.use(Router);
 
 export const router = new Router({
-  mode: 'history',
+  mode: "history",
   routes: [
-    { path: '/login', component: DefaultPage, props: { _meta: dataConfig.data._meta, data: dataConfig.data.login.data } },
-    { path: '/register', component: DefaultPage, props: { _meta: dataConfig.data._meta, data: dataConfig.data.register.data } },
-    { path: '/forgot-password', component: DefaultPage, props: { _meta: dataConfig.data._meta, data: dataConfig.data.forgotPassword.data } },
-    { path: '/change-password', component: DefaultPage, props: { _meta: dataConfig.data._meta, data: dataConfig.data.changePassword.data } },
-    { path: '/validate', component: DefaultPage, props: { _meta: dataConfig.data._meta, data: dataConfig.data.validate.data } },
+    {
+      path: "/api/users.js",
+    },
+    {
+      path: "/login",
+      component: DefaultPage,
+      props: {
+        _meta: dataConfig.data._meta,
+        initialData: dataConfig.data.login.data,
+        successHandle: { type: "redirect", url: "/login" },
+      },
+    },
+    {
+      path: "/register",
+      component: DefaultPage,
+      props: {
+        _meta: dataConfig.data._meta,
+        initialData: dataConfig.data.register.data,
+        successHandle: { type: "message" },
+      },
+    },
+    {
+      path: "/forgot-password",
+      component: DefaultPage,
+      props: {
+        _meta: dataConfig.data._meta,
+        initialData: dataConfig.data.forgotPassword.data,
+        successHandle: { type: "message" },
+      },
+    },
+    {
+      path: "/change-password",
+      component: DefaultPage,
+      props: {
+        _meta: dataConfig.data._meta,
+        initialData: dataConfig.data.changePassword.data,
+        successHandle: { type: "message" },
+      },
+    },
+    {
+      path: "/validate",
+      component: DefaultPage,
+      props: {
+        _meta: dataConfig.data._meta,
+        initialData: dataConfig.data.validate.data,
+      },
+    },
 
     // otherwise redirect to home
-    { path: '*', redirect: '/' }
-  ]
+    { path: "*", redirect: "/" },
+  ],
 });
 
 router.beforeEach((to, from, next) => {
   // redirect to login page if not logged in and trying to access a restricted page
-  const publicPages = ['/login', '/register', '/validate', '/forgot-password', '/change-password'];
+  const publicPages = [
+    "/api/users.js",
+    "/login",
+    "/register",
+    "/validate",
+    "/forgot-password",
+    "/change-password",
+  ];
   const authRequired = !publicPages.includes(to.path);
-  const loggedIn = localStorage.getItem('user');
+  const loggedIn = localStorage.getItem("user");
 
   if (authRequired && !loggedIn) {
-    return next('/login');
+    return next("/login");
   }
 
   next();
-})
+});

4f59a559c59a944c7b0d0e7ae7a9bf3faedc73c1
marcos-estrela <marcos.estrela@hotmail.com>
2020-09-17 01:07:09 -0300

    Add response, error-message and form-state treatment

---
+++ api/validate-user-key.js
@@ -0,0 +1,46 @@
+const lib = require("./common-lib");
+
+module.exports = async (req, res) => {
+  // Get a database connection, cached or otherwise,
+  // using the connection string environment variable as the argument
+  const db = await lib.connectToDatabase(process.env.MONGODB_URI);
+
+  // Select the "users" collection from the database
+  const collection = await db.collection("users");
+
+  // Select the users collection from the database
+  const user = await collection.findOne({ email: req.body.userKey });
+
+  if (!user) {
+    res.status(400).json({
+      message: "Esse usuário não existe em nossa base de dados.",
+    });
+    return;
+  }
+
+  if (user.mailValidation.key !== req.body.keycode) {
+    res.status(400).json({
+      message: "Esse código não é válido para esse usuário.",
+    });
+    return;
+  }
+
+  collection.updateOne(
+    { email: req.body.userKey },
+    {
+      $set: { "mailValidation.validated": true },
+    },
+    (error) => {
+      if (error) res.status(400).json({ error });
+      else {
+        res.status(200).json({
+          message:
+            "Obrigado por validar o seu e-mail. Agora você já pode utilizar o nosso sistema.",
+          type: "success",
+        });
+      }
+    }
+  );
+
+  // res.status(200).json({ user });
+};
--- src/pages/Default.vue
+++ src/pages/Default.vue
@@ -4,14 +4,16 @@
     :texts="data.texts"
     :inputs="data.inputs"
     :links="data.links"
+    :response="data.response"
     :validations="data.validations"
-    :errorMessages="data.errorMessages"
+    :error-messages="data.errorMessages"
+    :form-state="formState"
     @submit="handleSubmit"
   />
 </template>
 
 <script>
-import CommonPageComponent from "../components/CommonPage";
+import CommonPageComponent from "../components/CommonPage.vue";
 
 export default {
   components: {
@@ -20,26 +22,82 @@ export default {
 
   props: {
     _meta: Object,
-    data: Object,
+    initialData: Object,
+    successHandle: Object,
   },
 
-  created() {
-    const key = { key: this.$route.query.key };
-    if (this._meta.checkKey && key) console.warn("Chave não encontrada");
-    else {
-      this.$http
-        .post(`${this._meta.apiURL}${this.data.apiPath}`, key)
-        .then((response) => console.warn(response))
-        .catch((error) => console.warn(error));
+  data: function() {
+    var data = this.initialData;
+    data.response = {};
+    return {
+      data: data,
+      formState: "visible",
+    };
+  },
+
+  beforeMount() {
+    if (this.data.checkParameters) {
+      this.formState = "loading";
+      const key = this.$route.query.key;
+      const email = this.$route.query.email;
+      if (!key) {
+        this.data.response = {
+          type: "error",
+          message: "Chave não encontrada no link.",
+        };
+      } else if (!email) {
+        this.data.response = {
+          type: "error",
+          message: "Usuário não encontrado no link.",
+        };
+      } else {
+        this.$http
+          .post(`${this._meta.apiURL}${this.data.validationPath}`, {
+            key: key,
+            email: email,
+          })
+          .then(() => (this.formState = "visible"))
+          .catch((error) => {
+            this.data.response = {
+              type: "error",
+              message: error.response.data.message,
+            };
+            this.formState = "hidden";
+          });
+      }
     }
   },
 
   methods: {
-    handleSubmit(user) {
+    handleSubmit(data) {
+      data = this.addParameters(data);
       this.$http
-        .post(`${this._meta.apiURL}${this.data.apiPath}`, user)
-        .then((response) => console.warn(response))
-        .catch((error) => console.warn(error));
+        .post(`${this._meta.apiURL}${this.data.apiPath}`, data)
+        .then((response) => {
+          if (this.successHandle) {
+            if (this.successHandle.type === "redirect") {
+              this.$router.push(this.successHandle.url);
+            } else if (this.successHandle.type === "message") {
+              console.warn(response);
+              this.data.response = {
+                type: response.data.type,
+                message: response.data.message,
+              };
+            }
+          }
+        })
+        .catch((error) => {
+          this.data.response = {};
+          this.data.response = {
+            type: "error",
+            message: error.response.data.message,
+          };
+          console.warn(this.data.response);
+        });
+    },
+    addParameters(data) {
+      const queryParameters = this.$route.query;
+      return { ...data, ...queryParameters };
     },
   },
 };

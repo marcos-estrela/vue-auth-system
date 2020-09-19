309ee938a0b4bfa60f0f85e103ce3fb47aac98c0
marcos-estrela <marcos.estrela@hotmail.com>
2020-09-19 11:20:39 -0300

    Fix login api

--- api/login.js
+++ api/login.js
@@ -1,9 +1,10 @@
 const lib = require("./common-lib");
+import jwt from "njwt";
 
 module.exports = async (req, res) => {
   // Get a database connection, cached or otherwise,
   // using the connection string environment variable as the argument
-  const db = await connectToDatabase(process.env.MONGODB_URI);
+  const db = await lib.connectToDatabase(process.env.MONGODB_URI);
 
   if (!req.body.email) {
     res.status(400).json({ message: "E-mail é um campo obrigatório." });
@@ -18,20 +19,27 @@ module.exports = async (req, res) => {
   // Select the "users" collection from the database
   const collection = await db.collection("users");
 
+  const password = lib.crypto
+    .createHash("md5")
+    .update(req.body.password)
+    .digest("hex");
+
   // Select the users collection from the database
-  const user = await collection.find({
+  const user = await collection.findOne({
     email: req.body.email,
-    password: req.body.password,
+    password: password,
   });
-  if (user) {
+
+  if (!user) {
     res.status(400).json({
       message: "Usuário ou senha não correspondem a um usuário válido.",
     });
     return;
   }
 
-  const payload = { userKey: req.body.userKey };
+  const payload = { email: req.body.email };
   const token = jwt.create(payload, process.env.JWT_PHRASE);
   token.setExpiration(new Date().getTime() + 60 * 1000);
+
   res.status(200).json({ token: token });
 };

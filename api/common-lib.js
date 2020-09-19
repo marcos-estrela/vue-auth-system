f2c4046374509c432fca65c5250a3f798faba4de
marcos-estrela <marcos.estrela@hotmail.com>
2020-09-19 13:33:35 -0300

    Add try catch inside db connection and httpResponse function

--- api/common-lib.js
+++ api/common-lib.js
@@ -9,23 +9,42 @@ let cachedDb = null;
 
 // A function for connecting to MongoDB,
 // taking a single parameter of the connection string
-async function connectToDatabase(uri) {
+async function connectToDatabase(uri, response) {
   // If the database connection is cached,
   // use it instead of creating a new connection
   if (cachedDb) {
     return cachedDb;
   }
 
-  // If no connection is cached, create a new one
-  const client = await MongoClient.connect(uri, { useNewUrlParser: true });
+  try {
+    // If no connection is cached, create a new one
+    const client = await MongoClient.connect(uri, { useNewUrlParser: true });
 
-  // Select the database through the connection,
-  // using the database path of the connection string
-  const db = client.db(url.parse(uri).pathname.substr(1));
+    // Select the database through the connection,
+    // using the database path of the connection string
+    const db = client.db(url.parse(uri).pathname.substr(1));
 
-  // Cache the database connection and return the connection
-  cachedDb = db;
-  return db;
+    // Cache the database connection and return the connection
+    cachedDb = db;
+    return db;
+  } catch (error) {
+    httpResponse({
+      response: response,
+      type: "error",
+      code: 400,
+      message: "Não foi posssível se conectar ao banco de dados",
+    });
+  }
 }
 
-module.exports = { sgMail, crypto, connectToDatabase };
+async function httpResponse({ response, type, code, message }) {
+  var responseObject = {};
+  responseObject[type] = {
+    code: code,
+    message: message,
+  };
+  response.status(code).json(responseObject);
+  response.send();
+}
+
+module.exports = { sgMail, crypto, connectToDatabase, httpResponse };
